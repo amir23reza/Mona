8419843e3afed60de338fd8d4c39a154a7527704
seyf <seyf@DESKTOP-BIMQKQG>
2020-09-08 16:46:01 +0430

    completeUi

--- src/pages/photoGraphy.js
+++ src/pages/photoGraphy.js
@@ -1,76 +1,101 @@
-import React from 'react'
-import { MDBRow, MDBCol, MDBContainer } from 'mdbreact'
-
+import React from "react";
+import { MDBRow, MDBCol } from "mdbreact";
 
 const Photography = () => {
   return (
-    <div id="photography" >
-
-      <MDBRow className="sectionTitle" style={{ marginRight: '0px', marginLeft: '0px' }}>
+    <div id="photography">
+      <MDBRow
+        className="sectionTitle"
+        style={{ marginRight: "0px", marginLeft: "0px" }}
+      >
         <MDBCol className="ArtSection">
           <h1>photography</h1>
         </MDBCol>
       </MDBRow>
 
-      <MDBRow style={{ marginRight: '0px', marginLeft: '0px', paddingTop: '30px' }}>
+      <MDBRow
+        style={{ marginRight: "0px", marginLeft: "0px", paddingTop: "30px" }}
+      >
+        <MDBCol lg="3" md="6" sm="12">
+          <div class="image-border">
+            <img
+              class="photo"
+              src={require("../images/photography/7.jpg")}
+              alt="/"
+            />
+          </div>
+        </MDBCol>
 
         <MDBCol lg="3" md="6" sm="12">
-
           <div class="image-border">
-            <img class="photo" src={require("../images/photography/7.jpg")} />
+            <img
+              class="photo"
+              src={require("../images/photography/3.jpg")}
+              alt="/"
+            />
           </div>
-
         </MDBCol>
 
         <MDBCol lg="3" md="6" sm="12">
-
           <div class="image-border">
-            <img class="photo" src={require("../images/photography/3.jpg")} />
+            <img
+              class="photo"
+              src={require("../images/photography/8.jpg")}
+              alt="/"
+            />
           </div>
-
-
         </MDBCol>
 
-        <MDBCol lg="3" md="6" sm="12" >
-
+        <MDBCol lg="3" md="6" sm="12">
           <div class="image-border">
-            <img class="photo" src={require("../images/photography/8.jpg")} />
+            <img
+              class="photo"
+              src={require("../images/photography/6.jpg")}
+              alt="/"
+            />
           </div>
-
-        </MDBCol>
-
-        <MDBCol lg="3" md="6" sm="12" >
-          <div class="image-border">
-            <img class="photo" src={require("../images/photography/6.jpg")} />
-          </div>
-
         </MDBCol>
       </MDBRow>
 
-      <MDBRow style={{ marginRight: '0px', marginLeft: '0px', paddingTop: '20px', paddingBottom:'30px' }}>
-
+      <MDBRow
+        style={{
+          marginRight: "0px",
+          marginLeft: "0px",
+          paddingTop: "20px",
+          paddingBottom: "30px",
+        }}
+      >
         <MDBCol lg="3" md="6" sm="12">
           <div class="image-border">
-            <img class="photo" src={require("../images/photography/9.jpg")} />
+            <img
+              class="photo"
+              src={require("../images/photography/9.jpg")}
+              alt="/"
+            />
           </div>
-
         </MDBCol>
 
         <MDBCol lg="6" md="6" sm="12">
           <div class="image-border">
-            <img class="photo" src={require("../images/photography/12.jpg")} />
+            <img
+              class="photo"
+              src={require("../images/photography/12.jpg")}
+              alt="/"
+            />
           </div>
-
         </MDBCol>
         <MDBCol lg="3" md="6" sm="12">
           <div class="image-border">
-            <img class="photo" src={require("../images/photography/11.jpg")} />
+            <img
+              class="photo"
+              src={require("../images/photography/11.jpg")}
+              alt="/"
+            />
           </div>
-
         </MDBCol>
       </MDBRow>
     </div>
-  )
-}
+  );
+};
 
 export default Photography;
--- src/pages/uiProjects.js
+++ src/pages/uiProjects.js
@@ -4,28 +4,95 @@ const UIProjects = () => {
   return (
     <div>
       <MDBContainer>
-        <MDBRow style={{ marginRight: "0px", marginLeft: "0px" }}>
-          <h1>Ui-Ux Projects</h1>
+        <MDBRow
+          className="sectionTitle"
+          style={{ marginRight: "0px", marginLeft: "0px" }}
+        >
+          <MDBCol className="ArtSection">
+            <h1>UI-UX Projects</h1>
+          </MDBCol>
         </MDBRow>
-        <MDBRow style={{ marginRight: "0px", marginLeft: "0px" }}>
-          <MDBCol lg="12" style={{ padding: "20px 0" }}>
-            <h2>ibe project</h2>
+
+        <MDBRow
+          style={{ marginRight: "0px", marginLeft: "0px", marginTop: "20px" }}
+        >
+          <MDBCol lg="9" style={{ padding: "0px 10px" }}>
+            <div className="image-border">
+              <img
+                className="UiPhoto"
+                src={require("../images/2.PNG")}
+                alt="/"
+              />
+            </div>
           </MDBCol>
 
-          <MDBCol lg="9" style={{ padding: "10px 10px" }}>
-            <img className="UiPhoto" src={require("../images/2.PNG")} alt="/" />
+          <MDBCol lg="3">
+            <MDBRow>
+              <MDBCol lg="12">
+                <h2 className="UiTitle">IBE Project</h2>
+              </MDBCol>
+              <MDBCol>
+                <p className="descriptOne">
+                  It is a long established fact that a reader will be distracted
+                  by the readable content of a page when looking at its layout.
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters, as opposed to using 'Content
+                  here, content here', making it look like readable English.
+                  Many desktop publishing packages and web page editors now use
+                  Lorem Ipsum as their default model text, and a search for
+                  'lorem ipsum' will uncover many web sites still in their
+                  infancy.
+                </p>
+              </MDBCol>
+            </MDBRow>
           </MDBCol>
+        </MDBRow>
 
-          <MDBCol lg="3" className="descript">
-            <MDBRow style={{ marginRight: "0px", marginLeft: "0px" }}>
-              <MDBCol lg="12" className="descriptOne">
-                <h2>Client</h2>
+        <MDBRow
+          style={{ marginRight: "0px", marginLeft: "0px", paddingTop: "20px" }}
+        >
+          <MDBCol lg="12" className="descript">
+            <MDBRow
+              style={{
+                paddingTop: "10px",
+              }}
+            >
+              <MDBCol lg="3">
+                <img
+                  className="UiLogo"
+                  src={require("../images/client.png")}
+                  alt="/"
+                />
+                <h4 className="UiTitle">Client:</h4>
+                <p>Tehran University of Medical Science</p>
+              </MDBCol>
+              <MDBCol lg="3">
+                <img
+                  className="UiLogo"
+                  src={require("../images/team.png")}
+                  alt="/"
+                />
+                <h4 className="UiTitle">Team:</h4>
+                <p>Rayan Pajouh Salamat, Ui/Ux team</p>
+              </MDBCol>
 
-                <h2>Team</h2>
-
-                <h2>Date</h2>
-
-                <h2>Services</h2>
+              <MDBCol lg="3">
+                <img
+                  className="UiLogo"
+                  src={require("../images/date.png")}
+                  alt="/"
+                />
+                <h4 className="UiTitle">Date:</h4>
+                <p>Rayan Pajouh Salamat, Ui/Ux team</p>
+              </MDBCol>
+              <MDBCol lg="3">
+                <img
+                  className="UiLogo"
+                  src={require("../images/services.png")}
+                  alt="/"
+                />
+                <h4 className="UiTitle">Services:</h4>
+                <p>Rayan Pajouh Salamat, Ui/Ux team</p>
               </MDBCol>
             </MDBRow>
           </MDBCol>
